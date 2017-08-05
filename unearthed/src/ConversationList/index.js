b89618284c19827a689c191bb903228eb637e46f
Bensas <juanbensa@gmail.com>
2017-08-05 18:41:06 -0300

    MessagesScreen added

---
+++ unearthed/src/ConversationList/index.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, FlatList, Image} from 'react-native';
+
+import Conversation from './Conversation/index'
+
+const conversations = [
+  {
+      id: 1,
+      converserId: "40129158",
+      converserName:"Micho Gonzales",
+      lastMessagePreview: "Buenos días Aurelio, estamos interesados en tu oferta..."
+  },
+  {
+      id: 2,
+      converserId: "40129152",
+      converserName:"Tito Rodriguez",
+      lastMessagePreview: "Muchas gracias!"
+  },
+  {
+      id: 3,
+      converserId: "40129134",
+      converserName:"Cabezon Estevez",
+      lastMessagePreview: "Y dónde completaste tus estudios?"
+  }
+];
+
+export default class ConversationList extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={styles.title}>
+          Mensajes:
+        </Text>
+        <FlatList data={conversations} renderItem={item => <Conversation item={item.item}/>} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  title: {
+    fontSize: 30,
+    textAlign: 'center',
+  }
+});
