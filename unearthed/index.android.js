05467a6648c15f84530daab55ba3a7cacfa6eae8
Guido Princ <geprinc95@gmail.com>
2017-08-05 12:38:42 -0300

    JobsList with Items start

--- unearthed/index.android.js
+++ unearthed/index.android.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -12,20 +6,13 @@ import {
   View
 } from 'react-native';
 
+import JobsList from './src/jobList/index';
+
 export default class unearthed extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <JobsList />
       </View>
     );
   }
@@ -34,20 +21,9 @@ export default class unearthed extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'stretch',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
 AppRegistry.registerComponent('unearthed', () => unearthed);
