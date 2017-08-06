1b4596118ef9960255be9df9f85fdab437f7a1e0
Guido Princ <geprinc95@gmail.com>
2017-08-05 22:30:35 -0300

    Added navigation to the whole screens

---
+++ unearthed/src/screens.js
@@ -0,0 +1,39 @@
+import { PureComponent } from 'react';
+import { NavigationActions, TabNavigator, StackNavigator } from 'react-navigation';
+
+import Login from './screens/login';
+import JobsList from './screens/jobList';
+import JobOffer from './screens/jobOffer';
+import ConversationList from './screens/conversationList';
+import Profile from './screens/profile';
+import { NavigationOptions } from './NavigationOptions';
+
+export default StackNavigator({
+  Login: {
+    screen: Login,
+    navigationOptions: NavigationOptions.login
+  },
+  Home: {
+    screen: TabNavigator(
+      {
+        JobsList: {
+          screen: JobsList,
+          navigationOptions: NavigationOptions.jobsList
+        },
+        Profile: {
+          screen: Profile,
+          navigationOptions: NavigationOptions.profile
+        },
+        Conversations: {
+          screen: ConversationList,
+          navigationOptions: NavigationOptions.conversations
+        }
+      }
+    ),
+    navigationOptions: NavigationOptions.tabs
+  },
+  JobOffer: {
+    screen: JobOffer,
+    navigationOptions: NavigationOptions.jobOffer
+  }
+});
